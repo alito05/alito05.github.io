5cf384396141618a0af521f660ba5a4195cf0f3f
Alejandro Figueras <alejandro.figueras@valtech.com>
2022-09-24 03:52:42 -0300

    grid added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import NavBar from './components/Navbar/NavBar';
-import { Fragment } from 'react';
 import Home from './components/Home/Home';
+import Grid from './components/Products/Grid';
+import { Fragment } from 'react';
 
 function App() {
   return (
@@ -11,11 +12,7 @@ function App() {
         <NavBar/>
         <Routes>
             <Route path='/' element= {<Home/>}></Route>
-            <Route path='/men' element= {
-              <div>
-                Men
-              </div>
-            }></Route>
+            <Route path='/men' element= {<Grid/>}></Route>
             <Route path='/women' element= {<div>Women</div>}></Route>
             <Route path='/kids' element= {<div>Kids</div>}></Route>
             <Route path='/all' element= {<div>All</div>}></Route>
---
+++ src/components/Products/Grid.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Loading_icon from '../../media/img/Loading_icon.gif'
+
+const Grid = () => {
+  return (
+    <div className='flex flex-wrap gap-2 w-4/5 mx-auto'>
+        <div className='w-80 h-72 bg-black'>
+            {/*<img src={Loading_icon} className={'w-auto h-full'} alt="loading..." />*/}
+        </div>
+        <div className='w-80 h-72 bg-black'>{/*<img src={Loading_icon} className={'w-auto h-full'} alt="loading..." />*/}</div>
+        <div className='w-80 h-72 bg-black'>{/*<img src={Loading_icon} className={'w-auto h-full'} alt="loading..." />*/}</div>
+        <div className='w-80 h-72 bg-black'>{/*<img src={Loading_icon} className={'w-auto h-full'} alt="loading..." />*/}</div>
+        <div className='w-80 h-72 bg-black'>{/*<img src={Loading_icon} className={'w-auto h-full'} alt="loading..." />*/}</div>
+        <div className='w-80 h-72 bg-black'>{/*<img src={Loading_icon} className={'w-auto h-full'} alt="loading..." />*/}</div>
+        <div className='w-80 h-72 bg-black'>{/*<img src={Loading_icon} className={'w-auto h-full'} alt="loading..." />*/}</div>
+        <div className='w-80 h-72 bg-black'>{/*<img src={Loading_icon} className={'w-auto h-full'} alt="loading..." />*/}</div>
+        <div className='w-80 h-72 bg-black'>{/*<img src={Loading_icon} className={'w-auto h-full'} alt="loading..." />*/}</div>
+        <div className='w-80 h-72 bg-black'>{/*<img src={Loading_icon} className={'w-auto h-full'} alt="loading..." />*/}</div>
+        <div className='w-80 h-72 bg-black'>{/*<img src={Loading_icon} className={'w-auto h-full'} alt="loading..." />*/}</div>
+        <div className='w-80 h-72 bg-black'>{/*<img src={Loading_icon} className={'w-auto h-full'} alt="loading..." />*/}</div>
+        <div className='w-80 h-72 bg-black'>{/*<img src={Loading_icon} className={'w-auto h-full'} alt="loading..." />*/}</div>
+        <div className='w-80 h-72 bg-black'>{/*<img src={Loading_icon} className={'w-auto h-full'} alt="loading..." />*/}</div>
+        <div className='w-80 h-72 bg-black'>{/*<img src={Loading_icon} className={'w-auto h-full'} alt="loading..." />*/}</div>
+        <div className='w-80 h-72 bg-black'>{/*<img src={Loading_icon} className={'w-auto h-full'} alt="loading..." />*/}</div>
+        <div className='w-80 h-72 bg-black'>{/*<img src={Loading_icon} className={'w-auto h-full'} alt="loading..." />*/}</div>
+        <div className='w-80 h-72 bg-black'>{/*<img src={Loading_icon} className={'w-auto h-full'} alt="loading..." />*/}</div>
+        <div className='w-80 h-72 bg-black'>{/*<img src={Loading_icon} className={'w-auto h-full'} alt="loading..." />*/}</div>
+    </div>
+  )
+}
+
+export default Grid
